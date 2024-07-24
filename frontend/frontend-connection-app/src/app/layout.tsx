e9bedb058f8bee927477c6904ca684f2db9b938e
Elf <tokhtarkhan1967@gmail.com>
2024-07-24 13:24:26 +0500

    icon added

--- frontend/frontend-connection-app/src/app/layout.tsx
+++ frontend/frontend-connection-app/src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-LJJPTGCY1M`}
           strategy="afterInteractive"
